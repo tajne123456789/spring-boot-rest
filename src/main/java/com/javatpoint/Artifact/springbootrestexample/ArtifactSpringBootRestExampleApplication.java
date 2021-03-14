cb4b0cdc49779f15928a5904c5df238644a18155
Havelka <tajne123@gmail.com>
2021-03-14 18:45:48 +0100

    spring boot with rest

---
+++ src/main/java/com/javatpoint/Artifact/springbootrestexample/ArtifactSpringBootRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.Artifact.springbootrestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactSpringBootRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactSpringBootRestExampleApplication.class, args);
+	}
+
+}
