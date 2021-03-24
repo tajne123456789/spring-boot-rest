f173456485bc05f6a43f0aeeada9b47134e564a9
Havelka <tajne123@gmail.com>
2021-03-24 22:56:14 +0100

    oprava

--- src/main/java/com/javatpoint/SpringBootRestExampleApplication.java
+++ src/main/java/com/javatpoint/SpringBootRestExampleApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource(value="file:/com/myco/app.properties",ignoreResourceNotFound=true)
+@PropertySource(value="file:/app/config/application.properties",ignoreResourceNotFound=true)
 @SpringBootApplication
 public class SpringBootRestExampleApplication 
 {
